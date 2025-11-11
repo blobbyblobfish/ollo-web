86bafb8d9e4ad53e1aad13e4aebcd8ccf832d1a3
Alice Onuffer <aliceonuffer@Alices-MacBook-Pro.local>
2025-11-11 16:00:33 -0500

    About

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Nav from './Nav';
 import Home from './pages/Home';
-// import About from './pages/About';
+import About from './pages/About';
 import './App.css';
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
